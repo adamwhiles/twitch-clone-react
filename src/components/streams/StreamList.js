b3ad3076b5fc17ce83682d00360a232bbd2addba
Adam Whiles <awhiles2011@gmail.com>
2019-01-02 15:52:00 -0600

    fixed typo in action creator

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -6,6 +6,13 @@ class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
+
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return <div>EDIT/DELETE</div>;
+    }
+  }
+
   renderList() {
     return this.props.streams.map(stream => {
       return (
@@ -15,6 +22,7 @@ class StreamList extends React.Component {
             {stream.title}
             <div className="description">{stream.description}</div>
           </div>
+          {this.renderAdmin(stream)}
         </div>
       );
     });
@@ -30,7 +38,10 @@ class StreamList extends React.Component {
 }
 
 const mapStateToProps = state => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId
+  };
 };
 
 export default connect(
